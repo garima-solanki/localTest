7236dcc9f1bd9be69b82854cc06fff6e7c9b6052
garima.solanki <garima.solanki@GARIMA-SOLANKI>
2016-12-14 18:55:19 +0530

    page changes

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/public');
-//app.set('view engine', 'ejs');
+app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
   response.render('public/index');
