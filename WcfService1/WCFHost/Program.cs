d754f020516f0bd6916785b8a06864140119afd2
garima.solanki <garima.solanki@GARIMA-SOLANKI>
2017-02-14 14:52:45 +0530

    WCF

---
+++ WcfService1/WCFHost/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+
+namespace WCFHost
+{
+    class Program
+    {
+        static void Main()
+        {
+            using (ServiceHost host = new ServiceHost(typeof(WcfServiceLibrary1.Service1)))
+            {
+                host.Open();
+                Console.WriteLine("Service started @ " + DateTime.Now.ToString());
+                Console.ReadLine();
+            }
+        }
+    }
+}
